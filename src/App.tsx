dd5c0f637bab622566acc207d8de027f24e6ba6c
zsaveleva <zsaveleva>
2024-01-06 21:09:58 +0300

    Delete Provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import "./App.css";
 import { Todo, rootStore } from "./store";
-import { Provider, observer } from "mobx-react";
+import { observer } from "mobx-react";
 
 const App = observer(() => {
   const [todo, setTodo] = useState<string>("");
@@ -11,47 +11,45 @@ const App = observer(() => {
     rootStore.todos.add(todo);
     setTodo("");
   };
-  
+
   return (
-    <Provider value={rootStore}>
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          gap: 10,
-          maxWidth: 300,
-          margin: "auto",
-          marginTop: "100px",
-        }}
-      >
-        <input value={todo} onChange={(e) => setTodo(e.target.value)} />
-        <button onClick={addTodo}>Add todo</button>
-        {rootStore.todos
-          .getAll()
-          .reverse()
-          .map((todo: Todo) => {
-            return (
-              <div
-                style={{ display: "flex", alignItems: "center", gap: "4px" }}
-                key={todo.id}
-              >
-                <input
-                  id={todo.id.toString()}
-                  type="checkbox"
-                  checked={todo.completed}
-                  onChange={() => {
-                    todo.toggle();
-                  }}
-                />
-                <label htmlFor={todo.id.toString()}>{todo.title}</label>
-                <button onClick={() => rootStore.todos.delete(todo.id)}>
-                  Delete
-                </button>
-              </div>
-            );
-          })}
-      </div>
-    </Provider>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        gap: 10,
+        maxWidth: 300,
+        margin: "auto",
+        marginTop: "100px",
+      }}
+    >
+      <input value={todo} onChange={(e) => setTodo(e.target.value)} />
+      <button onClick={addTodo}>Add todo</button>
+      {rootStore.todos
+        .getAll()
+        .reverse()
+        .map((todo: Todo) => {
+          return (
+            <div
+              style={{ display: "flex", alignItems: "center", gap: "4px" }}
+              key={todo.id}
+            >
+              <input
+                id={todo.id.toString()}
+                type="checkbox"
+                checked={todo.completed}
+                onChange={() => {
+                  todo.toggle();
+                }}
+              />
+              <label htmlFor={todo.id.toString()}>{todo.title}</label>
+              <button onClick={() => rootStore.todos.delete(todo.id)}>
+                Delete
+              </button>
+            </div>
+          );
+        })}
+    </div>
   );
 });
 
